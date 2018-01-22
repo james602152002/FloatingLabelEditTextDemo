7343631149cf5974278f6d7e579ce1db7e23bd89
= <=>
2018-01-22 15:38:54 +0800

    update

--- app/src/main/java/com/james602152002/floatinglabeledittextdemo/MainActivity.java
+++ app/src/main/java/com/james602152002/floatinglabeledittextdemo/MainActivity.java
@@ -2,20 +2,30 @@ package com.james602152002.floatinglabeledittextdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
 
 import com.james602152002.floatinglabeledittext.FloatingLabelEditText;
 import com.james602152002.floatinglabeledittext.validator.RegexValidator;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private FloatingLabelEditText label_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FloatingLabelEditText editText = findViewById(R.id.label_1);
-        editText.addValidator(new RegexValidator("long error hint", "\\d+"));
-        editText.addValidator(new RegexValidator("You input letters.", "[A-Za-z]+$"));
-//        editText.setHint_text_color(Color.BLUE);
-//        editText.setError("error");
+        label_1 = findViewById(R.id.label_1);
+        label_1.addValidator(new RegexValidator("long error hint", "\\d+"));
+        label_1.addValidator(new RegexValidator("You input letters.", "[A-Za-z]+$"));
+//        label_1.setHint_text_color(Color.BLUE);
+//        label_1.setError("error");
+        AppCompatButton button = findViewById(R.id.submit);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        label_1.setError("submit error");
     }
 }
